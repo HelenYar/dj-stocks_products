617ac6001e2b4f3647ddcbd990f5cd077e6c2f27
HelenYar <helen.yarosh82@gmail.com>
2021-11-22 22:06:16 +0300

    hw_dj_6

---
+++ logistic/views.py
@@ -0,0 +1,25 @@
+from rest_framework.viewsets import ModelViewSet
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+    # filter_backends = [SearchFilter]
+    # search_fields = ['title', 'description']
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'description']
+    pagination_class = PageNumberPagination
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['address', 'products']
